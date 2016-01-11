3702934d6ed6a5bf961f8b919c580477e1caf4f9
Steven Spriggs <steven.spriggs@gmail.com>
2016-01-11 16:32:12 -0500

    switch to normalize.scss

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ gulp.task('todo', function(){
       ])
       .pipe(sourcemaps.init())
       .pipe(sass({
-        includePaths: ['scss'],
+      includePaths: ['scss','./bower_components/support-for/sass'],
         outputStyle: 'expanded'
       }))
       .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7", { cascade: true }))
