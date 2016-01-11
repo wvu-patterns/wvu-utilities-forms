bdf3e28236601b147e293a8b2491108b36fab5d9
Steven Spriggs <steven.spriggs@gmail.com>
2016-01-11 16:32:26 -0500

    use manifest.scss pattern

--- gulpfile.js
+++ gulpfile.js
@@ -12,16 +12,16 @@ var gulp = require('gulp'),
     reload = browserSync.reload;
 
 
-  gulp.task('browser-sync', function() {
-    browserSync({
-      server: {
-        baseDir: "./build",
-      },
-      open: false,
-      logConnections: true,
-      logSnippet: false
-    });
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: "./build",
+    },
+    open: false,
+    logConnections: true,
+    logSnippet: false
   });
+});
 
 gulp.task('todo', function(){
   return gulp.src([
@@ -38,42 +38,45 @@ gulp.task('todo', function(){
   .pipe(gulp.dest('./'));
 });
 
-  gulp.task('compile-scss', function(){
-    return gulp.src([
-        './test/scss/styles.scss'
-      ])
-      .pipe(sourcemaps.init())
-      .pipe(sass({
+gulp.task('compile-scss', function(){
+  return gulp.src([
+      './test/scss/styles.scss'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
       includePaths: ['scss','./bower_components/support-for/sass'],
-        outputStyle: 'expanded'
-      }))
-      .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7", { cascade: true }))
-      .pipe(sourcemaps.write('maps', {
-        includeContent: false,
-        sourceRoot: './build/css/'
-      }))
-      .pipe(gulp.dest('./build/css/'));
-  });
+      outputStyle: 'expanded'
+    }))
+    .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7", { cascade: true }))
+    .pipe(sourcemaps.write('maps', {
+      includeContent: false,
+      sourceRoot: './build/css/'
+    }))
+    .pipe(gulp.dest('./build/css/'));
+});
 
-  gulp.task('compile-handlebars', function () {
+gulp.task('build', ['todo','compile-scss'], function () {
 
-    var templateData = JSON.parse(fs.readFileSync('./data/_wvu-forms.json'));
+  var templateData = JSON.parse(fs.readFileSync('./data/_wvu-forms.json'));
 
-    var options = {
-      batch : [
-        './test/partials'
-      ]
-    };
+  var options = {
+    batch : [
+      './test/partials'
+    ]
+  };
 
-    return gulp.src('./test/index.hbs')
-          .pipe(handlebars(templateData, options))
-          .pipe(rename('index.html'))
-          .pipe(gulp.dest('./build'));
-  });
+  return gulp.src('./test/index.hbs')
+        .pipe(handlebars(templateData, options))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('./build'));
+});
 
-  gulp.task('default',['compile-scss','compile-handlebars','browser-sync'], function(){
-    gulp.watch(["./src/*.scss","./test/scss/*.scss"],["compile-scss"]);
-    gulp.watch(["./test/**/*.hbs","./test/data.json"],["compile-handlebars"]);
-    gulp.watch("./build/**/*.html").on('change',reload);
-    gulp.watch("./build/css/*.css").on('change',reload);
-  });
+gulp.task('ci',['build']);
+
+gulp.task('default',['build','browser-sync'], function(){
+  gulp.watch(["./src/*.scss","./test/scss/*.scss"],["compile-scss"]);
+  gulp.watch(["./test/**/*.hbs","./test/data.json"],["build"]);
+  gulp.watch("./build/**/*.html").on('change',reload);
+  gulp.watch("./build/css/*.css").on('change',reload);
+  gulp.watch(['./src/haml/**/*.haml','./src/cleanslate/**/*.html'], ['todo']);
+});
